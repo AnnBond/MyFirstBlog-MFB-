39af8f202de8ebbac409617eb2c0e11147343de9
AnnBond <annbondar4uk@gmail.com>
2017-08-03 00:04:03 +0300

    Connection to database

---
+++ db.php
@@ -0,0 +1,9 @@
+<?php
+try {
+    $DBH = new PDO("mysql:host=localhost;dbname=blog_project", 'root', '');
+    $DBH->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+}
+catch (PDOException $e) {
+    echo $e->getMessage();
+}
+
