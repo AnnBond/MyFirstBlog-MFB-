87345e53b9c9d5df5633f5289638e4c881e92f15
AnnBond <annbondar4uk@gmail.com>
2017-08-05 01:05:08 +0300

    Update check adding post

--- add.php
+++ add.php
@@ -1,12 +1,21 @@
 <?php
 include_once('db.php');
 
+
 if (isset($_POST['add'])) {
-    $STH = $DBH->prepare("INSERT INTO post (title, description, date) VALUES (:title, :description, CURRENT_DATE())");
-    $STH->bindParam(":title", $_POST["title"]);
-    $STH->bindParam(":description", $_POST["description"]);
-    $STH->execute();
-    header('Location:index.php');
+    if (!empty($_POST['title']) && !empty($_POST['description'])) {
+        $STH = $DBH->prepare("INSERT INTO post (title, description, date) VALUES (:title, :description, CURRENT_DATE())");
+        $STH->bindParam(":title", $_POST["title"]);
+        $STH->bindParam(":description", $_POST["description"]);
+        $STH->execute();
+        header('Location:index.php');
+    } else {
+        echo '<style>
+        .errors {
+        display: block !important;
+        }
+        </style>';
+    }
 }
 
 include ('header.php');
@@ -14,13 +23,14 @@ include ('header.php');
             <section class="addPost clearfix">
                 <form class="newPost" method="post" action="add.php">
                     <label>Title: <br>
-                        <input type="text" class="add-post-title" name="title" placeholder="Title of post" required />
+                        <input type="text" class="add-post-title" name="title" placeholder="Title of post"  />
                     </label>
                     <label>Post:
-                        <textarea name="description" cols="74" rows="10" placeholder="Some interesting..."></textarea>
+                        <textarea name="description" cols="74" rows="10" placeholder="Some interesting..." ></textarea>
                     </label>
                     <button type="submit" class="submit" name="add">Submit</button>
                 </form>
+                <p class="errors">Fill all inputs, please !!!!!!!!!!!!!!!!!!!</p>
             </section>
         </div>
     </body>
